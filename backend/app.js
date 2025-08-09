1f6bb542cab3d00009839f0ee665aac5a53554c5
Jils31 <jilspatel21@gmail.com>
2025-08-09 13:58:17 +0530

    Added some info for the paths in backend

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,21 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        message: "Welcome to the Backend of the Portfolio Analytics Dashboard",
+        availableEndpoints: {
+            holdings: "/api/portfolio/holdings",
+            allocation: "/api/portfolio/allocation",
+            performance: "/api/portfolio/performance",
+            summary: "/api/portfolio/summary",
+            overview:"/api/portfolio/overview"
+        },
+        note: "Append these paths to the base URL to access different sections of the portfolio data."
+    });
+});
+
+
 //Routes for the required endpoints
 const portfolioRoutes = require('./routes/portfolioRoutes')
 app.use('/api/portfolio', portfolioRoutes)
